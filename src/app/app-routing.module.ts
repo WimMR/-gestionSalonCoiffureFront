3d42d72d9baf0c1fc6745f95b8a1c4d26d479305
WimMR <wim.ricci@gmail.com>
2023-05-26 14:14:46 +0200

    modification des pages html utilisateur et avis

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UtilisateurComponent } from './utilisateur/utilisateur/utilisateur.component';
+import { AvisComponent } from './Avis/avis/avis.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'utilisateur', // localhost:4200/utilisateur
+    component:UtilisateurComponent,
+  },
+  {
+    path:'avis', // localhost:4200/utilisateur
+    component:AvisComponent 
+    
+    
+  }
+  
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

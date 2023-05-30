d9af16e00313719b8d11e65d495bc52c4ec59530
WimMR <wim.ricci@gmail.com>
2023-05-30 09:43:16 +0200

    ajout lien reservation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UtilisateurComponent } from './utilisateur/utilisateur/utilisateur.component';
 import { AvisComponent } from './Avis/avis/avis.component';
+import { ReservationComponent } from './reservation/reservation.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,12 @@ const routes: Routes = [
   {
     path:'avis', // localhost:4200/utilisateur
     component:AvisComponent 
+    
+    
+  },
+  {
+    path:'reservations', // localhost:4200/utilisateur
+    component:ReservationComponent 
     
     
   }
